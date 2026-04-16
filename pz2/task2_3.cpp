1137d2f526f0ef45b7d8efd1a08e9fb433f3cb53
iMer <i.merschanskiy@gmail.com>
2026-04-16 20:42:38 +0300

    Task 2.3 done. Code + documentation for several functions.

---
+++ pz2/task2_3.cpp
@@ -0,0 +1,142 @@
+//
+// Знайти мін. парне число головної діагоналі матриці.
+// Формування, друк і обробка двовимірних масивів.
+// Реалізувати масиви як псевдодінамічні.
+// Формування масивів виконати з використанням ГСЧ.
+// В масиви записувати і +, і - числа.
+//
+#include <iostream>
+#include <iomanip> // setw()
+#include <cstdlib> // system("cls"), RAND_MAX 
+#include <random>
+
+using namespace std;
+
+int** createMatrix(int n, int m);
+void fillMatrix(int** matrix, int n, int m);
+void minEvenValueMainD(int** matrix, int n);
+void printMatrix(int** matrix, int n, int m);
+void deleteMatrix(int** matrix, int n);
+
+int main() {
+    system("cls");
+    cout << "Введіть кількість рядків (n) матриці: ";
+    int n; // rows
+    if (!(cin >> n)) {
+        cerr << "\nНеправильний ввід.\n";
+        cout << "Спробуйте ціле позитивне число.\n";
+        return 1;
+    }
+    if (n <= 0) {
+        cerr << "\nМатриця не може мати таку кількість рядків.\n";
+        cout << "Спробуйте ввести ціле позитивне число.\n";
+        return 1;
+    }
+
+    cout << "Введіть кількість рядків (m) матриці: ";
+    int m; // columns
+    if (!(cin >> m)) {
+        cerr << "\nНеправильний ввід.\n";
+        cout << "Спробуйте ціле позитивне число.\n";
+        return 1;
+    }
+    if (m <= 0) {
+        cerr << "\nМатриця не може мати таку кількість стовпців.\n";
+        cout << "Спробуйте ввести ціле позитивне число.\n";
+        return 1;
+    }
+
+    int **matrix = createMatrix(n, m);
+    
+    fillMatrix(matrix, n, m);
+
+    minEvenValueMainD(matrix, min(n, m));
+
+    printMatrix(matrix, n, m);
+
+    deleteMatrix(matrix, n);
+    return 0;
+}
+
+/**
+ * @brief Allocates memory for a 2D dynamic array (matrix).
+ * @param n number of rows in the matrix.
+ * @param m number of columns in the matrix.
+ * @return Pointer to the newly allocated 2D array of integers.
+ * @note Allocated memory should be deallocated using deleteMatrix().
+ */
+int** createMatrix(int n, int m) {
+    int **matrix = new int *[n];
+    for (int i = 0; i < n; i++) matrix[i] = new int[m];
+    return matrix;
+}
+
+/**
+ * @brief Fills the matrix with random integers from -RAND_MAX to RAND_MAX.
+ * @param matrix pointer to a 2D dynamic array of integers.
+ * @param n number of rows in the matrix.
+ * @param m number of columns in the matrix.
+ * @return Void.
+ * @note Uses the Mersenne Twister (mt19937) algorithm for random number generation.
+ * https://en.wikipedia.org/wiki/Mersenne_Twister.
+ * @note The generator is static to ensure program performance by being created once during the first call.
+ */
+void fillMatrix(int** matrix, int n, int m) {
+    //
+    // {} – create the object
+    // () – calls object
+    //
+    static mt19937 gen(random_device{}());
+    uniform_int_distribution<> distrib(-RAND_MAX, RAND_MAX);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix[i][j] = distrib(gen);
+        }
+    }
+}
+
+/**
+ * @brief Find the minimal even value on the matrix main diagonal.
+ * @param matrix pointer to a 2D dynamic array of integers.
+ * @param n number of elements of main diagonal (dimension).
+ * @return Void. The result is printed to the console.
+ * @note If no even numbers are found, a corresponding message is displayed.
+ */
+void minEvenValueMainD(int** matrix, int n) {
+    int minEl = RAND_MAX + 1;
+    for (int i = 0; i < n; i++) {
+        if (minEl > matrix[i][i] && matrix[i][i] % 2 == 0) {
+            minEl = matrix[i][i];
+        }
+    }
+    if (minEl != RAND_MAX + 1) {
+        cout << "\nМінімальне парне число головної діагоналі: " 
+            << minEl << "\n";
+    } else { cout << "\nНа головній діагоналі немає парних чисел.\n"; }
+
+}
+
+void printMatrix(int** matrix, int n, int m) {
+    cout << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << "| ";
+        for (int j = 0; j < m; j++) {
+            cout << setw(6) << matrix[i][j] << " ";
+        }
+        cout << "|\n";
+    }
+    cout << "\n";
+}
+
+/**
+ * @brief Deallocates memory used by the 2D array.
+ * @param matrix pointer to a 2D dynamic array of integers.
+ * @param n number of rows in the matrix.
+ */
+void deleteMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) delete[] matrix[i];
+    delete[] matrix;
+
+    cout << "\nМатрицю видалено.\n";
+}
