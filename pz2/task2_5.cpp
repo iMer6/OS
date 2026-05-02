9c985d59f4531cc614f7bccbc76c935fc70089a1
iMer <i.merschanskiy@gmail.com>
2026-05-02 16:25:08 +0300

    First part in task 2.5 done.

--- pz2/task2_5.cpp
+++ pz2/task2_5.cpp
@@ -11,24 +11,45 @@
 */
 #include <iostream>
 #include <string>
-#include <stdexcept>
 
 using std::cin;
 using std::cout;
 using std::cerr;
 
-std::string substr(const std::string& str, size_t startIndex, int n);
+std::string substr(const std::string& str, size_t startIndex = 0, int n = -1);
 
 int main() {
+    // Reading input text.
     std::string txt;
     cout << "Введіть рядок символів: ";
     std::getline(cin, txt);
-    int wordCount = 1;
-    for (char ch : txt) { if (ch == ' ') wordCount++; }
 
+    if (txt.empty()) {
+        cout << "\nРядок порожній.";
+        return 0;
+    }
+
+    // Counting words and checking for non-space characters.
+    size_t wordCount = 1;
+    bool isSymbol = false;
+    for (size_t i = 0; i < txt.length() - 1; i++) {
+        if (txt[i] == ' ' && txt[i + 1] != ' ') wordCount++;
+        if (txt[i] != ' ') isSymbol = true;
+    }
+
+    if (!isSymbol) {
+        cout << "\nУ рядку немає символів.";
+        return 0;
+    }
+
+    // Spliting the text into an array of words.
     std::string* words = new std::string[wordCount]();
-    for (int i = 0, j = 0; i < txt.length(); i++) {
-        if (txt[i] == ' ' && i == 0) continue;
+    for (size_t i = 0, j = 0; i < txt.length(); i++) {
+        if (txt[0] == ' ') {
+            txt = substr(txt, 1, -1);
+            i = 0;
+            continue;
+        }
         if (txt[i] == ' ' && i != 0) {
             words[j++] = substr(txt, 0, i);
             txt = substr(txt, i + 1, -1);
@@ -37,33 +58,41 @@ int main() {
     }
     words[wordCount - 1] = txt;
 
-    for (int i = 0; i < wordCount; i++) { cout << words[i] << " "; }
+    // Removing every even words in text. 
+    txt = "";
+    for (size_t i = 1; i < wordCount; i += 2) txt += words[i] + " ";
+    
+    // Output the result.
+    cout << "\n";
+    for (char ch : txt) cout << ch;
 
-    cout << "\nКількість слів у рядку: " << wordCount;
-
+    delete[] words;
+    words = nullptr;
     return 0;
 }
 
 /**
  * @brief Construst and return a new string.
- * @param str – entry string.
- * @param startIndex – index of first character in string (default 0).
- * @param n – number of characters in substring (default remainder).
+ * @param str entry string.
+ * @param startIndex index of first character in string (default 0). Must be in [0, string length).
+ * @param n number of characters in substring (default remainder). Must be in [0, string length - startIndex].
  * @return The new string.
- * @throw std::out_of_range if startIndex < 0 or startIndex >= string length.
- * @throw std::out_of_range if n < 0 or n >= string length - startIndex.
  * @note Parameter startIndex optional. Default value is 0 (str start).
  * @note Parameter n optional. Default value is remainder.
 */
-std::string substr(const std::string& str, size_t startIndex = 0, int n = -1) {
+std::string substr(const std::string& str, size_t startIndex, int n) {
     size_t len = str.length();
 
     if (startIndex < 0 || startIndex >= len) {
-        throw std::out_of_range("Значення startIndex поза межами рядка.");
+        cerr << "Значення startIndex поза межами рядка.";
+        return str;
     }
 
     if (n == -1 || startIndex + n > len) n = len - startIndex;
-    if (n < 0) throw std::out_of_range("Неправильна кількість символів.");
+    if (n < 0) { 
+        cerr << "Неправильна кількість символів.";
+        return str;
+    }
     
     std::string newStr;
     for (int i = 0; i < n; i++) newStr+= str[startIndex + i];
